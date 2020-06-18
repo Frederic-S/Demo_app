f6c8067964e066bd0bb5aed4b0d604132565ba76
Fred-S <fredvantage@gmail.com>
2020-06-18 16:08:40 +0200

    addad navbar

---
+++ lib/Navbar/Navbar.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class Navbar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth > 1200) {
+          return DesktopNavbar();
+        } else if (constraints.maxWidth > 800 && constraints.maxWidth < 1200) {
+          return DesktopNavbar();
+        } else {
+          return MobileNavbar();
+        }
+      },
+    );
+  }
+}
+
+class DesktopNavbar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        children: <Widget>[
+          Text(
+            "Null Pole",
+            style: TextStyle(
+                fontWeight: FontWeight.bold, color: Colors.white, fontSize: 30),
+          ),
+          Row(
+            children: <Widget>[
+              Text(
+                "Home",
+                style: TextStyle(color: Colors.white),
+              ),
+              Text(
+                "About Us",
+                style: TextStyle(color: Colors.white),
+              ),
+              Text(
+                "Portfolio",
+                style: TextStyle(color: Colors.white),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class MobileNavbar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
